2b5d0ef11a51dba2a836c44447858c41348c0072
SCOTT A. EXTON <scotte@au1.ibm.com>
2021-07-29 20:21:30 +1000

    Add the snapshotmgr to the controller.

--- src/controllers/ibmsecurityverifyaccess_controller.go
+++ src/controllers/ibmsecurityverifyaccess_controller.go
@@ -1,20 +1,10 @@
 /*
-Copyright 2021 isamdev@au1.ibm.com.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright contributors to the IBM Security Verify Access Operator project
+ */
 
 package controllers
+
+/*****************************************************************************/
 
 import (
 	appsv1 "k8s.io/api/apps/v1"
@@ -22,12 +12,14 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+
 	"reflect"
 	"time"
-
 	"context"
+	"net/http"
 
 	"github.com/go-logr/logr"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -35,12 +27,20 @@ import (
 	ibmv1 "github.com/ibm-security/verify-access-operator/api/v1"
 )
 
-// IBMSecurityVerifyAccessReconciler reconciles a IBMSecurityVerifyAccess object
+/*****************************************************************************/
+
+/*
+ * The IBMSecurityVerifyAccessReconciler structure reconciles an 
+ * IBMSecurityVerifyAccess object.
+ */
+
 type IBMSecurityVerifyAccessReconciler struct {
 	client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
 }
+
+/*****************************************************************************/
 
 //+kubebuilder:rbac:groups=ibm.com,resources=ibmsecurityverifyaccesses,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=ibm.com,resources=ibmsecurityverifyaccesses/status,verbs=get;update;patch
@@ -48,85 +48,119 @@ type IBMSecurityVerifyAccessReconciler struct {
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the IBMSecurityVerifyAccess object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
-func (r *IBMSecurityVerifyAccessReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+/*****************************************************************************/
+
+/*
+ * Reconcile is part of the main kubernetes reconciliation loop which aims to
+ * move the current state of the cluster closer to the desired state.
+ *
+ * For more details, check Reconcile and its Result here:
+ * - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+ */
+
+func (r *IBMSecurityVerifyAccessReconciler) Reconcile(
+		ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = r.Log.WithValues("ibmsecurityverifyaccess", req.NamespacedName)
 
 	// Fetch the instance
 	verifyaccess := &ibmv1.IBMSecurityVerifyAccess{}
-	err := r.Get(ctx, req.NamespacedName, verifyaccess)
+	err          := r.Get(ctx, req.NamespacedName, verifyaccess)
+
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// The requested object was not found.  It could have been deleted 
+			// after the reconcile request.  Owned objects are automatically 
+			// garbage collected. For additional cleanup logic use finalizers.
 			// Return and don't requeue
-			r.Log.Info("VerifyAccess resource not found. Ignoring since object must be deleted")
+			r.Log.Info("VerifyAccess resource not found. " +
+						"Ignoring since object must be deleted")
 			return ctrl.Result{}, nil
 		}
+
 		// Error reading the object - requeue the request.
 		r.Log.Error(err, "Failed to get VerifyAccess")
+
 		return ctrl.Result{}, err
 	}
 
-	// Check if the deployment already exists, if not create a new one
+	// Check if the deployment already exists, if not create a new one.
 	found := &appsv1.Deployment{}
-	err = r.Get(ctx, types.NamespacedName{Name: verifyaccess.Name, Namespace: verifyaccess.Namespace}, found)
+	err    = r.Get(
+					ctx,
+					types.NamespacedName{
+						Name:      verifyaccess.Name,
+						Namespace: verifyaccess.Namespace},
+					found)
+
 	if err != nil && errors.IsNotFound(err) {
 		// Define a new deployment
 		dep := r.deploymentForVerifyAccess(verifyaccess)
-		r.Log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		r.Log.Info("Creating a new Deployment", "Deployment.Namespace",
+						dep.Namespace, "Deployment.Name", dep.Name)
+
 		err = r.Create(ctx, dep)
+
 		if err != nil {
-			r.Log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			r.Log.Error(err, "Failed to create new Deployment",
+							"Deployment.Namespace", dep.Namespace,
+							"Deployment.Name", dep.Name)
+
 			return ctrl.Result{}, err
 		}
-		// Deployment created successfully - return and requeue
+
+		// Deployment created successfully - return and requeue.
 		return ctrl.Result{Requeue: true}, nil
+
 	} else if err != nil {
 		r.Log.Error(err, "Failed to get Deployment")
 		return ctrl.Result{}, err
 	}
 
-	// Ensure the deployment size is the same as the spec
+	// Ensure that the deployment size is the same as the spec.
 	size := verifyaccess.Spec.Size
+
 	if *found.Spec.Replicas != size {
 		found.Spec.Replicas = &size
 		err = r.Update(ctx, found)
 		if err != nil {
-			r.Log.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
+			r.Log.Error(err, "Failed to update Deployment",
+						"Deployment.Namespace", found.Namespace,
+						"Deployment.Name", found.Name)
 			return ctrl.Result{}, err
 		}
+
 		// Ask to requeue after 1 minute in order to give enough time for the
 		// pods be created on the cluster side and the operand be able
 		// to do the next update step accurately.
 		return ctrl.Result{RequeueAfter: time.Minute}, nil
 	}
 
-	// Update the VerifyAccess status with the pod names
-	// List the pods for this verifyaccess's deployment
+	// Update the VerifyAccess status with the pod names and list the pods for 
+	// this verifyaccess's deployment
+
 	podList := &corev1.PodList{}
-	listOpts := []client.ListOption{
+
+	listOpts := []client.ListOption {
 		client.InNamespace(verifyaccess.Namespace),
 		client.MatchingLabels(labelsForVerifyAccess(verifyaccess.Name)),
 	}
+
 	if err = r.List(ctx, podList, listOpts...); err != nil {
-		r.Log.Error(err, "Failed to list pods", "VerifyAccess.Namespace", verifyaccess.Namespace, "VerifyAccess.Name", verifyaccess.Name)
+		r.Log.Error(err, "Failed to list pods", "VerifyAccess.Namespace",
+				verifyaccess.Namespace, "VerifyAccess.Name", verifyaccess.Name)
+
 		return ctrl.Result{}, err
 	}
+
 	podNames := getPodNames(podList.Items)
 
-	// Update status.Nodes if needed
+	// Update status.Nodes, if needed.
+
 	if !reflect.DeepEqual(podNames, verifyaccess.Status.Nodes) {
 		verifyaccess.Status.Nodes = podNames
+
 		err := r.Status().Update(ctx, verifyaccess)
+
 		if err != nil {
 			r.Log.Error(err, "Failed to update VerifyAccess status")
 			return ctrl.Result{}, err
@@ -136,9 +170,15 @@ func (r *IBMSecurityVerifyAccessReconciler) Reconcile(ctx context.Context, req c
 	return ctrl.Result{}, nil
 }
 
-// deploymentForVerifyAccess returns a VerifyAccess Deployment object
-func (r *IBMSecurityVerifyAccessReconciler) deploymentForVerifyAccess(m *ibmv1.IBMSecurityVerifyAccess) *appsv1.Deployment {
-	ls := labelsForVerifyAccess(m.Name)
+/*****************************************************************************/
+
+/*
+ * The following function is used to return a VerifyAccess Deployment object.
+ */
+
+func (r *IBMSecurityVerifyAccessReconciler) deploymentForVerifyAccess(
+					m *ibmv1.IBMSecurityVerifyAccess) *appsv1.Deployment {
+	ls       := labelsForVerifyAccess(m.Name)
 	replicas := m.Spec.Size
 
 	dep := &appsv1.Deployment{
@@ -169,30 +209,78 @@ func (r *IBMSecurityVerifyAccessReconciler) deploymentForVerifyAccess(m *ibmv1.I
 			},
 		},
 	}
-	// Set VerifyAccess instance as the owner and controller
+
+	// Set the VerifyAccess instance as the owner and controller
 	ctrl.SetControllerReference(m, dep, r.Scheme)
+
 	return dep
 }
 
-// labelsForVerifyAccess returns the labels for selecting the resources
-// belonging to the given VerifyAccess CR name.
+/*****************************************************************************/
+
+/*
+ * The following function returns the labels which are used when selecting
+ * the resources belonging to the given VerifyAccess CR name.
+ */
+
 func labelsForVerifyAccess(name string) map[string]string {
 	return map[string]string{"app": "VerifyAccess", "VerifyAccess_cr": name}
 }
 
-// getPodNames returns the pod names of the array of pods passed in
+/*****************************************************************************/
+
+/*
+ * The following function returns the pod names of the supplied array of pods.
+ */
+
 func getPodNames(pods []corev1.Pod) []string {
 	var podNames []string
+
 	for _, pod := range pods {
 		podNames = append(podNames, pod.Name)
 	}
+
 	return podNames
 }
 
-// SetupWithManager sets up the controller with the Manager.
+/*****************************************************************************/
+
+/*
+ * The following function is used to set up the controller with the Manager.
+ */
+
 func (r *IBMSecurityVerifyAccessReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// start the snapshot manager
+	go r.startSnapshotMgr(mgr)
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&ibmv1.IBMSecurityVerifyAccess{}).
 		Owns(&appsv1.Deployment{}).
 		Complete(r)
 }
+
+/*****************************************************************************/
+
+/*
+ * This function is used to setup and start the snapshot manager server. 
+ */
+
+func (r *IBMSecurityVerifyAccessReconciler) startSnapshotMgr(mgr ctrl.Manager) {
+	_ = r.Log.WithValues("snapshotmgr", "Server")
+
+	r.Log.Info("Starting the snapshot manager...")
+
+	// Setup the snapshot manager.
+	snapshot := &SnapshotMgr{
+		server: &http.Server{
+			Addr: ":7443",
+		},
+		log: r.Log.WithValues("SnapshotMgr", "Server"),
+	}
+
+	// Start the snapshot manager.
+	snapshot.start()
+}
+
+/*****************************************************************************/
+
